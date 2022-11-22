f201476bbd18a93919b230d402df5abe5509a5ac
xuanco941 <xuanco941@gmail.com>
2022-11-22 11:59:05 +0700

    init DAL

---
+++ ManagementSoftware/DAL/DALLoiLoc.cs
@@ -0,0 +1,24 @@
+﻿using ManagementSoftware.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSoftware.DAL
+{
+    public class DALLoiLoc
+    {
+        public static void Add(TestLoiLoc testLoiLoc ,LoiLoc loiLoc)
+        {
+            DataBaseContext dbContext = new DataBaseContext();
+
+            dbContext.TestLoiLocs.Add(testLoiLoc);
+            dbContext.SaveChanges();
+
+            loiLoc.TestLoiLocID = testLoiLoc.TestLoiLocID;
+            dbContext.LoiLocs.Add(loiLoc);
+            dbContext.SaveChanges();
+        }
+    }
+}
