a9d0007f80d3dac4cbb1de2bf79e2e4d5352312a
xuanco941 <xuanco941@gmail.com>
2022-12-07 16:22:58 +0700

    add change strong connect db

--- ManagementSoftware/GUI/Section/FormStringConnectDatabase.Designer.cs
+++ ManagementSoftware/GUI/Section/FormStringConnectDatabase.Designer.cs
@@ -33,6 +33,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -47,10 +49,10 @@
             this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(464, 126);
+            this.button1.Location = new System.Drawing.Point(506, 88);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 35);
+            this.button1.Size = new System.Drawing.Size(106, 41);
             this.button1.TabIndex = 16;
             this.button1.Text = "Xác nhận";
             this.button1.TextColor = System.Drawing.Color.White;
@@ -60,50 +62,75 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(167, 114);
+            this.label1.Location = new System.Drawing.Point(96, 73);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 17);
+            this.label1.Size = new System.Drawing.Size(188, 20);
             this.label1.TabIndex = 17;
-            this.label1.Text = "Chuỗi kết nối (SQL Server)";
+            this.label1.Text = "Chuỗi kết nối (SQL Server) :";
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(167, 132);
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(96, 93);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(290, 29);
+            this.textBox1.Size = new System.Drawing.Size(403, 33);
             this.textBox1.TabIndex = 18;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(167, 173);
+            this.label2.Location = new System.Drawing.Point(96, 134);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(383, 13);
+            this.label2.Size = new System.Drawing.Size(397, 15);
             this.label2.TabIndex = 19;
             this.label2.Text = "VD : Data Source=DESKTOP-P4IC2M8\\SQLEXPRESS;Initial Catalog=Mutosi;";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(167, 186);
+            this.label3.Location = new System.Drawing.Point(96, 147);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.Size = new System.Drawing.Size(154, 15);
             this.label3.TabIndex = 20;
             this.label3.Text = "User ID=sa;Password=12345";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label4.Location = new System.Drawing.Point(96, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(373, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Nếu lỗi permission denied hãy mở Notepad bằng quyền administrator, ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label5.Location = new System.Drawing.Point(96, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(436, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "open file ConnectStringDB.txt trong folder chứa ứng dụng và thay chuỗi kết nối ấy" +
+    ".";
             // 
             // FormStringConnectDatabase
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
-            this.ClientSize = new System.Drawing.Size(721, 277);
+            this.ClientSize = new System.Drawing.Size(711, 259);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
@@ -124,5 +151,7 @@
         private TextBox textBox1;
         private Label label2;
         private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
